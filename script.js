01230c82cb0a85eb65c96941a2713d9662bb8712
Muhammad <myousuf432@gmail.com>
2025-06-12 20:34:07 +0100

    Added Check Button logic,Structed JS with section comments, and implemented random number generation

--- script.js
+++ script.js
@@ -6,9 +6,25 @@ const returnToHomeButton = document.getElementById("returnToHomeButton");
 const howToPlayPage = document.getElementById("howToPlay");
 const returnFromGame = document.getElementById("returnFromGame");
 const guessBox = document.getElementsByClassName("guessBox")
+const checkButton = document.getElementById("checkButton");
 
 
-// Visual Display of Pages
+// =========================
+// Random Number Generation 
+// =========================
+
+const correctCode = [];
+
+for (let i = 0; i < 4; i++) {
+    const randomDigit = Math.floor(Math.random() * 10);
+    correctCode.push(string(randomDigit));
+
+}
+
+// =========================
+// Visual Display of Pages 
+// =========================
+
 
 
 playButton.addEventListener("click", function () {
@@ -36,8 +52,9 @@ returnFromGame.addEventListener("click", function () {
 
 
 
-
+// =========================
 // Guess Box Logic
+// =========================
 
 
 //Logic for keypresses and build current guess to max 4 digits
@@ -50,22 +67,53 @@ document.addEventListener("keydown", function (event) {
     if ("0123456789".includes(event.key) &&
 
         currentGuess.length < 4) {
-        currentGuess.push(event.key)
+        currentGuess.push(event.key);
     }
 
 
     if (event.key === "Backspace" || event.key === "Delete") {
-        currentGuess.pop()
+        currentGuess.pop();
     }
 
+
+    // Logic for looping through guess boxes
+
+    Array.from(guessBox).forEach((box, index) => {
+        box.innerText = currentGuess[index] || "";
+        // ||  if current index exists, use it, otherwise just use an emptry string to clear the box
+    });
+
     console.log(currentGuess)
-})
 
-
-// Logic for looping through guess boxes
-
-Array.from(guessBox).forEach((box, index) => {
-    box.innerText = currentGuess[index] || "";
-    // ||  if current index exists, use it, otherwise just use an emptry string to clear the box
 });
+
+
+// =========================
+// Check Box Logic
+// =========================
+
+
+checkButton.addEventListener("click", function () {
+
+    if (currentGuess.length === 4) {
+
+        let allCorrect = true;
+
+        for (let i = 0; i < 4; i++) {
+            if (
+                currentGuess[i] !== correctCode[i]) {
+                allCorrect = false;
+                break;
+            }
+
+        }
+        if (allCorrect) {
+            gamePage.style.display = "none";
+            successPage.style.display = "block";
+        }
+    }
+
+
+});
+
 
